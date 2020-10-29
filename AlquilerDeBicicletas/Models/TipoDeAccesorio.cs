f20a21fe197ea769f1498691dce3a40b4dfc1373
Alan Gao <1982548075@qq.com>
2020-10-28 22:17:09 -0300

    Agregacion de la clase TipoDeAccesorio

---
+++ AlquilerDeBicicletas/Models/TipoDeAccesorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlquilerDeBicicletas.Models
+{
+    public class TipoDeAccesorio
+    {
+        public int idTipoDeAccesorio { get; set; }
+        public string nombre { get; set; }
+        public double precio { get; set; }
+    }
+}

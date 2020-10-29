02fab1cc2a285d209119a51f01938ae2c86fe284
Alan Gao <1982548075@qq.com>
2020-10-28 21:58:40 -0300

    Agrego atributos ala clase usuario

--- AlquilerDeBicicletas/Models/Bicicleta.cs
+++ AlquilerDeBicicletas/Models/Bicicleta.cs
@@ -7,13 +7,7 @@ namespace AlquilerDeBicicletas.Models
 {
     public class Bicicleta
     {
-        public int idUsuario { get; set; }
-        public String nombre { get; set; }
-        public String apellido { get; set; }
-        public String dni { get; set; }
-        public String eMail { get; set; }
-        public DateTime fechaNacimiento { get; set; }
-        public String contrasena { get; set; }
+
 
     }
 }
--- AlquilerDeBicicletas/Models/Usuario.cs
+++ AlquilerDeBicicletas/Models/Usuario.cs
@@ -7,5 +7,12 @@ namespace AlquilerDeBicicletas.Models
 {
     public class Usuario
     {
+        public int idUsuario { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public String dni { get; set; }
+        public String eMail { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public String contrasena { get; set; }
     }
 }

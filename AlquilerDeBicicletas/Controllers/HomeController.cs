ee8ee6270336adfb639bb67ff39a27b29a37ca2f
Alan Gao <1982548075@qq.com>
2020-12-02 21:00:08 -0300

    Agregacion del filtro en el indice

--- AlquilerDeBicicletas/Controllers/HomeController.cs
+++ AlquilerDeBicicletas/Controllers/HomeController.cs
@@ -5,15 +5,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AlquilerDeBicicletas.Models;
+using AlquilerDeBicicletas.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AlquilerDeBicicletas.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AlquilerDeBicisDatabseContext _context;
+
+        public HomeController(AlquilerDeBicisDatabseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int tipoDeBici)
+        {
+            ViewData["tipoDeBiciID"] = new SelectList(_context.TiposDeBici, "tipoDeBiciID", "nombre");
+            var alquilerDeBicisDatabseContext = _context.Bicicletas.Include(b => b.tipoDeBici);
+
+            if (tipoDeBici != 0)
+            {   //https://docs.microsoft.com/es-es/ef/ef6/querying/related-data
+                alquilerDeBicisDatabseContext = _context.Bicicletas.Where(b => b.tipoDeBici.tipoDeBiciID == tipoDeBici).Include(b => b.tipoDeBici);
+            }
+            return View(await alquilerDeBicisDatabseContext.ToListAsync());
+        }
+        //public async Task<IActionResult> Filtrar(TipoDeBici tipoDeBici)
+        //{
+            
+        //    return View(await alquilerDeBicisDatabseContext.ToListAsync());
+        //}
+        /*public IActionResult Index()
         {
             return View();
-        }
+        }*/
 
         public IActionResult About()
         {
---
+++ AlquilerDeBicicletas/Migrations/20201128201411_primerMigracion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlquilerDeBicicletas.Migrations
+{
+    public partial class primerMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
